ad36e9630625dfbbbe57c42f653274a8ff75fb4e
liansun96 <toetetaung3296@gmail.com>
2023-06-14 10:47:29 +0630

    update nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BiSearch } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
+import { FiChevronDown } from "react-icons/fi";
 import { navLinks } from "../constants/index";
+import { Animated } from "react-animated-css";
 
 const Nav = () => {
   return (
@@ -27,10 +30,80 @@ const Nav = () => {
           <div className="flex items-center gap-10">
             <ul className="flex gap-8">
               {navLinks?.map((navLink) => (
-                <li className="text-heading_text text-sm tracking-wider cursor-pointer duration-500 hover:text-dark_blue" key={navLink.id}>
+                <li
+                  className="mt-[3px] text-heading_text text-sm tracking-wider cursor-pointer duration-300 hover:text-dark_blue"
+                  key={navLink.id}
+                >
                   {navLink.title}
                 </li>
               ))}
+              <li>
+                <div className="group relative inline-flex">
+                  <div
+                    className="flex items-center text-heading_text text-sm tracking-wider cursor-pointer duration-300 hover:text-dark_blue"
+                    href=""
+                  >
+                    Pages
+                    <FiChevronDown className="ml-1" />
+                  </div>
+                  <span className="group-hover:block hidden cursor-pointer w-[260px] py-7 z-10 absolute top-5 -left-4">
+                    <div className="bg-primary border border-header_border custom-shoadow-lg px-3 py-4">
+                      <ul className="text-heading_text text-sm tracking-wider space-y-[3px]">
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Activity
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Category
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Ranking
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Creators
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Market Single
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Live Bidding
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Create Item
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Author Profile
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Login & Register
+                        </li>
+                      </ul>
+                    </div>
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div className="group relative inline-flex">
+                  <div
+                    className="flex items-center text-heading_text text-sm tracking-wider cursor-pointer duration-300 hover:text-dark_blue"
+                    href=""
+                  >
+                    Blog
+                    <FiChevronDown className="ml-1" />
+                  </div>
+                  <span className="group-hover:block hidden cursor-pointer w-[260px] py-7 z-10 absolute top-5 -left-4">
+                    <div className="bg-primary border border-header_border custom-shoadow-lg px-3 py-4">
+                      <ul className="text-heading_text text-sm tracking-wider space-y-[3px]">
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Our Blog
+                        </li>
+                        <li className="hover:text-dark_blue px-2 py-2 duration-300">
+                          Blog Details
+                        </li>
+                      </ul>
+                    </div>
+                  </span>
+                </div>
+              </li>
             </ul>
             <div className="transform duration-1000 transition hover:bg-gradient-to-l from-[#cc42e4] to-[#6345ed] bg-gradient-to-r from-[#bc3cd3] to-[#6345ed] p-1 rounded-full">
               <div className="border border-[#fff] border-dashed bg-transparent rounded-3xl">
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,22 +1,14 @@
 export const navLinks = [
-    {
-      id: "home",
-      title: "Home",
-    },
-    {
-      id: "explore",
-      title: "Explore",
-    },
-    {
-      id: "collection",
-      title: "Collection",
-    },
-    {
-      id: "pages",
-      title: "Pages",
-    },
-    {
-        id: "blog",
-        title: "Blog",
-      },
-  ];
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "explore",
+    title: "Explore",
+  },
+  {
+    id: "collection",
+    title: "Collection",
+  },
+];
