67a56f798a2c7bb8b039452db8dbd5939fd24cd7
chanmyae-aung <chanmyaeaung.cmg@gmail.com>
2023-06-14 21:37:07 +0630

    update

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -7,11 +7,11 @@ import Info from "./Info";
 
 const Blog = () => {
   return (
-    <main className=" bg-[#070B24] md:ml-[110px]">
+    <main className=" bg-[#070B24] md:pl-[110px]">
       <div className=" w-[100%] h-96 md:h-screen  bg-no-repeat flex justify-center items-center " style={{ backgroundImage: `url(${background})`}}>
         <h2 className="text-4xl text-white font-semibold mt-20 md:mt-0">Latest News</h2>
       </div>
-      <div className="md:absolute bottom-20 md:bottom-16 w-full ">
+      <div className="md:absolute bottom-20 md:bottom-16  ">
         <MidBar/>
       </div>
       <div className="mt-5 md:mt-0">
