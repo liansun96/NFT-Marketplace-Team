61df6b40838bf0f454102e9712074a6e420455bc
liansun96 <toetetaung3296@gmail.com>
2023-06-19 14:35:08 +0630

    apply sitebar links

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import appsDelete from "../../icons/apps-delete.svg";
 import bookAlt from "../../icons/book-alt.svg";
 import butterfly from "../../icons/butterfly.svg";
@@ -9,6 +9,51 @@ import toolMarquee from "../../icons/tool-marquee.svg";
 import "./Sidebar.css";
 import { NavLink } from "react-router-dom";
 const Sidebar = () => {
+  const [fullScreen, setFullScreen] = useState(false);
+
+  const handleFullScreenClick = () => {
+    const element = document.documentElement;
+
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+  };
+
+  const handleExitFullScreenClick = () => {
+    if (
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement
+    ) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
+  };
+
+  const toggleFullScreen = () => {
+    if (fullScreen) {
+      handleFullScreenClick();
+      setFullScreen(!fullScreen);
+    } else {
+      handleExitFullScreenClick();
+      setFullScreen(!fullScreen);
+    }
+  };
+
   return (
     <div className="flex flex-row md:flex-col px-6 py-3 w-screen h-[90px] lg:w-[110px] lg:h-screen bg-primary  fixed -bottom-2 lg:top-0 left-0 custom-shadow z-10 overflow-x-scroll">
       <ul className="flex flex-row gap-4 md:justify-center items-center md:flex-col md:gap-6 md:mt-6">
@@ -33,33 +78,46 @@ const Sidebar = () => {
         </li>
         <li>
           <div className="w-[50px] md:w-[55px] rounded-[9px] border border-icon_border p-3 transition ease-out hover:icon-bg bg-secondary">
-            <img className="w-6 md:w-8 " src={bookAlt} alt="" />
+            <NavLink to={"/explore"}>
+              <img className="w-6 md:w-8 " src={bookAlt} alt="" />
+            </NavLink>
           </div>
         </li>
         <div className="hidden md:block mx-auto ms-3 md:w-[30px] md:py-[3px] bg-secondary rounded-[3px]"></div>
         <div className="block md:hidden mx-auto h-[30px] px-[3px] bg-secondary rounded-[3px]"></div>
         <li>
           <div className="w-[50px] md:w-[55px] rounded-[9px] border border-icon_border p-3 transition ease-out hover:icon-bg bg-secondary">
-            <img className="w-6 md:w-8 " src={butterfly} alt="" />
+            <NavLink to={"/collection"}>
+              <img className="w-6 md:w-8 " src={butterfly} alt="" />
+            </NavLink>
           </div>
         </li>
         <li>
           <div className="w-[50px] md:w-[55px] rounded-[9px] border border-icon_border p-3 transition ease-out hover:icon-bg bg-secondary">
-            <img className="w-6 md:w-8 " src={capming} alt="" />
+            <NavLink to={"/marketsingle"}>
+              <img className="w-6 md:w-8 " src={capming} alt="" />
+            </NavLink>
           </div>
         </li>
         <li>
           <div className="w-[50px] md:w-[55px] rounded-[9px] border border-icon_border p-3 transition ease-out hover:icon-bg bg-secondary">
-            <img className="w-6 md:w-8 " src={crown} alt="" />
+            <NavLink to={"/blog"}>
+              <img className="w-6 md:w-8 " src={crown} alt="" />
+            </NavLink>
           </div>
         </li>
         <li>
           <div className="w-[50px] md:w-[55px] rounded-[9px] border border-icon_border p-3 transition ease-out hover:icon-bg bg-secondary">
-            <img className="w-6 md:w-8 " src={settings} alt="" />
+            <NavLink to={"/"}>
+              <img className="w-6 md:w-8 " src={settings} alt="" />
+            </NavLink>
           </div>
         </li>
         <li className="me-5 md:me-0 md:!mt-[40px]">
-          <div className="w-[50px] md:w-[55px] rounded-[9px] border border-icon_border p-3 transition ease-out hover:icon-bg bg-secondary">
+          <div
+            onClick={toggleFullScreen}
+            className="w-[50px] md:w-[55px] rounded-[9px] border border-icon_border p-3 transition ease-out hover:icon-bg bg-secondary"
+          >
             <img className="w-6 md:w-8 " src={toolMarquee} alt="" />
           </div>
         </li>
